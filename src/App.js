960fbb5d72ff6c14c7c1ae073df51b36372ceacd
Max <65555180+DiscordMax@users.noreply.github.com>
2020-09-03 18:55:42 -0400

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import './App.css';
+import { Switch, Route } from 'react-router-dom';
+import { LandingPage, MenuPage, DashboardPage } from './pages';
+
+function App() {
+    return ( <
+        Switch >
+        <
+        Route path = "/"
+        exact = { true }
+        component = { LandingPage }
+        /> <
+        Route path = "/dashboard"
+        exact = { true }
+        component = { MenuPage }
+        /> <
+        Route path = "/dashboard/:id"
+        exact = { true }
+        component = { DashboardPage }
+        /> < /
+        Switch >
+    );
+}
+
+export default App;
